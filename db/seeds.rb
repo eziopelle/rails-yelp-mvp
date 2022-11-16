8ef865e71921d1e99ffad734ac090b1f2a5da10e
Victor Pelle <victor.pelle97@gmail.com>
2022-11-16 13:03:35 +0100

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_name, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
